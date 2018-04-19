e56f6135ac971644465b36d4e7096a4b60dfcd2e
Andy Kwok <jliangguo@gmail.com>
2018-04-19 22:42:41 +0800

    kick off bugs

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -23,7 +23,7 @@ object List {
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x, xs) => x * product(ds)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] =
@@ -33,7 +33,7 @@ object List {
   val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // Matched
     case Cons(h, t) => h + sum(t)
     case _ => 101
   }
