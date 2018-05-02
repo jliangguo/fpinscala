eb1800da33e9ec8151c375adf1416a9a16973cad
Andy Kwok <jliangguo@gmail.com>
2018-05-02 22:03:42 +0800

    add laziness chapter code
    infinite stream and corecursive function

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -47,7 +47,7 @@ sealed trait Stream[+A] {
 
   @annotation.tailrec
   final def drop(n: Int): Stream[A] = this match {
-    case Cons(_, t) if n > 0 => t().drop(n-1)
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
     case _ => this
   }
 
@@ -59,7 +59,7 @@ sealed trait Stream[+A] {
   // 更通用的递归实现
   def foldRight[B](z: => B)(f: (A, => B) => B): B =
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
       case _ => z
     }
 
@@ -76,25 +76,87 @@ sealed trait Stream[+A] {
 
   def takeWhile_1(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((h, t) =>
-    if (f(h)) cons(h, t)
-    else empty)
+      if (f(h)) cons(h, t)
+      else empty)
 
   def headOption_1: Option[A] =
     foldRight(None: Option[A])((h, _) => Some(h))
 
   def map[B](f: A => B): Stream[B] =
-    foldRight(empty[B])((h,t) => cons(f(h), t))
+    foldRight(empty[B])((h, t) => cons(f(h), t))
 
   def filter(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((h, t) =>
-    if (f(h)) cons(h, t)
-    else t)
+      if (f(h)) cons(h, t)
+      else t)
 
   def append[B >: A](s: => Stream[B]): Stream[B] =
-    foldRight(empty[B])((h,t) => cons(h,t))
+    foldRight(empty[B])((h, t) => cons(h, t))
 
   def flatMap[B](f: A => Stream[B]): Stream[B] =
-    foldRight(empty[B])((h,t) => f(h) append t)
+    foldRight(empty[B])((h, t) => f(h) append t)
+
+  def mapViaUnfold[B](f: A => B): Stream[B] =
+    unfold(this) {
+      case Cons(h, t) => Some((f(h()), t()))
+      case _ => None
+    }
+
+  def takeViaUnfold(n: Int): Stream[A] =
+    unfold((this, n)) {
+      case (Cons(h, t), 1) => Some((h(), (empty, 0)))
+      case (Cons(h, t), n) if n > 1 => Some((h(), (t(), n - 1)))
+      case _ => None
+    }
+
+  def takeWhileViaUnfold(f: A => Boolean): Stream[A] =
+    unfold(this) {
+      case Cons(h, t) if f(h()) => Some((h(), t()))
+      case _ => None
+    }
+
+  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] =
+    unfold((this, s2)) {
+      case (Cons(h1, t1), Cons(h2, t2)) =>
+        Some((f(h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+
+  def zip[B](s2: Stream[B]): Stream[(A, B)] =
+    zipWith(s2)((_, _))
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
+    zipWithAll(s2)((_, _))
+
+  def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+    Stream.unfold((this, s2)) {
+      case (Empty, Empty) => None
+      case (Cons(h, t), Empty) => Some(f(Some(h()), Option.empty[B]) -> (t(), empty[B]))
+      case (Empty, Cons(h, t)) => Some(f(Option.empty[A], Some(h())) -> (empty[A] -> t()))
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
+    }
+
+  def startsWith[A](s: Stream[A]): Boolean =
+    zipAll(s).takeWhile(_._2.isDefined) forAll {
+      case (h, h2) => h == h2
+    }
+
+  def tails: Stream[Stream[A]] =
+    unfold(this) {
+      case Empty => None
+      case s => Some((s, s drop 1))
+    } append Stream(empty)
+
+  def hasSubSequence[A](s: Stream[A]): Boolean =
+    tails exists (_ startsWith s)
+
+  // should keep the accumulated value
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      lazy val p1 = p0 // using lazy val to ensure only one evaluation...
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
 }
 
 case object Empty extends Stream[Nothing]
@@ -117,5 +179,39 @@ object Stream {
   // 无限流-infinite stream
   val ones: Stream[Int] = Stream.cons(1, ones)
 
+  def constant_1[A](a: A): Stream[A] = Stream.cons(a, constant(a))
 
+  //  more efficient than constant_1
+  def constant[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+  }
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  // 斐波那契数列
+  val fibs = {
+    def go(f0: Int, f1: Int): Stream[Int] =
+      cons(f0, go(f1, f0 + f1))
+
+    go(0, 1)
+  }
+
+  // 共递归函数-corecursive
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((h, s)) => cons(h, unfold(s)(f))
+      case None => empty
+    }
+
+  val fibsViaUnfold =
+    unfold((0, 1)) { case (f0, f1) => Some((f0, (f1, f0 + f1))) }
+
+  def fromViaUnfold(n: Int) =
+    unfold(n)(n => Some((n, n + 1)))
+
+  def constantViaUnfold[A](a: A) =
+    unfold(a)(_ => Some((a, a)))
+
+  val onesViaUnfold = unfold(1)(_ => Some((1, 1)))
 }
